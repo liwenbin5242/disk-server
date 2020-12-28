2e0f0d61423282173c9d8e1ee333c4dd6c219ebe
liwenbin <liwenbin@iyunxiao.com>
2020-12-28 14:16:21 +0800

    add tieba module

---
+++ modules/tieba.js
@@ -0,0 +1,30 @@
+const mongodber = require('../utils/mongodber');
+const wecahtDB = mongodber.use('wechat');
+
+
+/**
+ * 外部接口调用,获取最新内容
+ */
+async function getText() {
+     
+}
+
+/**
+ * 外部接口调用,获取最新内容
+ */
+async function getTitle() {
+
+}
+
+/**
+ * 外部接口调用,获取沙发
+ */
+async function getSofa() {
+    const 
+}
+
+module.exports = {
+    getText,
+    getTitle,
+    getSofa
+}
