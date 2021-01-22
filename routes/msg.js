7e3c709f95d0498727e066b48f77610622e3a433
liwenbin <liwenbin@iyunxiao.com>
2021-01-22 10:50:41 +0800

    add

--- routes/msg.js
+++ routes/msg.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const {reqHandler} = require('../utils/reqHandler');
 const msgServ = require('../modules/msg');
 
-router.get('/yun', reqHandler(async function(req, res) {
+router.post('/yun', reqHandler(async function(req, res) {
     const result = await msgServ.getMsg(req.body);
     fs.writeFileSync(Date.now + '.txt', JSON.stringify(req.body));
     res.end(result);
