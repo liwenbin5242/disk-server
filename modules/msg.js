5be0e689fbdc0835b8e35fb8118ea700fcf54501
liwenbin <liwenbin@iyunxiao.com>
2021-01-30 10:44:50 +0800

    add

--- modules/msg.js
+++ modules/msg.js
@@ -1,15 +1,19 @@
 
 const {logger} = require('../utils/logger');
 const enums = require('../lib/enums');
+const mongodber = require('../utils/mongodber');
+const wechatDB = mongodber.use('wechat');
 
+const logicServ = require('./logic');
 /**
  * deal msg
  * @param {msg data} data 
  */
 function getMsg(data) {
+    wechatDB.collection('messages').insertOne(data);
     switch (data.messageType) {
     case enums.messageCodes.FriendRequest: 
-        
+        logicServ.dealFriendRequest();
     }
 }
 
