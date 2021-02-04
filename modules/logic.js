1bc2370c0a5ec868b7a2ca49bfc573ac75a8f006
liwenbin <liwenbin@iyunxiao.com>
2021-02-04 16:20:18 +0800

    post

--- modules/logic.js
+++ modules/logic.js
@@ -19,10 +19,10 @@ async function dealFriendRequest(reqData) {
             return resolve(result);             
         });
     });
-    logger.info(JSON.stringify(jsonData));
+    logger.info((jsonData));
     // 发送消息给我
     const msgData = {
-        content: `来自${jsonData.fromusername}的好友添加请求，请及时处理!!`
+        content: `来自${jsonData.msg.$.fromusername}的好友添加请求,请求备注${jsonData.remark}，请及时处理!!`
     };
     await wechatServ.postSendText(msgData);
     return jsonData;
