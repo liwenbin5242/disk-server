84cb88345a9df5fd082bfc45cd664bead0b52f93
liwenbin <liwenbin@iyunxiao.com>
2021-06-07 16:50:18 +0800

    add logger

--- modules/forcast.js
+++ modules/forcast.js
@@ -6,6 +6,7 @@ const puppeteer = require('puppeteer');
 const moment = require('moment');
 const wechatServ = require('./wechat');
 const {logger} = require('../utils/logger');
+const {FORCAST_ROOMS} = require('../lib/enums');
 moment.locale('zh-cn');
 
 /**
@@ -14,9 +15,10 @@ moment.locale('zh-cn');
 async function sendForcast() {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
+    logger.info('forcast begin');
     page.setViewport({width: 920, height: 1250}); // 设置视窗大小为 920*1250 适应手机尺寸
     try {
-        for (let room of config.get('FORCAST_ROOMS')) {
+        for (let room of FORCAST_ROOMS) {
             await page.goto(`http://weathernew.pae.baidu.com/weathernew/pc?query=${room.address.province}${room.address.city}天气&srcid=4982&city_name=${room.address.city}&province_name=${room.address.province}`);
             await page.screenshot({ path: `./public/${moment().format('YYYY-MM-DD')}.png` });
             setTimeout(async()=> {
